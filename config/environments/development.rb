0510aa38d2364e77390c58f1d8952cd71dd25f85
Sam O'Keefe <samok13@gmail.com>
2016-02-24 14:03:44 -0800

    emails working locally

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,8 +4,10 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  config.action_mailer.default_url_options = { :host => 'localhost' }
   config.action_mailer.delivery_method = :letter_opener
-  
+  Paperclip.options[:command_path] = "/usr/local/bin"
+
   config.paperclip_defaults = {
 
     # Don't forget to make S3 your storage option!
@@ -61,7 +63,5 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
-  Paperclip.options[:command_path] = "/usr/local/bin"
   
 end
